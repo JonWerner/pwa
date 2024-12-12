f6238065c5b0079c2cf443e4b7809748037e4a26
JonWerner <jon@thewerners.org>
2024-12-12 13:40:59 -0600

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,17 @@
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open('qr-handler-v1').then((cache) => {
+      return cache.addAll(['/index.html', '/app.js', '/styles.css']);
+    })
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  if (event.request.url.startsWith('https://qr.eventmagic.co')) {
+    event.respondWith(fetch(event.request));
+  } else {
+    event.respondWith(
+      caches.match(event.request).then((response) => response || fetch(event.request))
+    );
+  }
+});
